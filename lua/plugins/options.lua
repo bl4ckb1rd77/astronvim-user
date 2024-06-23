da193a037a6bd0043961dcb801e519dfca0e9bd3
Andre Höfer <bl4ckb1rd1977@googlemail.com>
2024-06-23 13:05:11 +0200

    tryed options to set relativnumbers to false but did not work

---
+++ lua/plugins/options.lua
@@ -0,0 +1,15 @@
+return {
+  "AstroNvim/astrocore",
+  ---@type AstroCoreOpts
+  opts = {
+    options = {
+      opt = { -- vim.opt.<key>
+        relativenumber = false, -- sets vim.opt.relativenumber
+        number = true, -- sets vim.opt.number
+        spell = false, -- sets vim.opt.spell
+        signcolumn = "yes", -- sets vim.opt.signcolumn to yes
+        wrap = false, -- sets vim.opt.wrap
+      },
+    },
+  },
+}

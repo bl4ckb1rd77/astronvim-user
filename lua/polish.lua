d18a4534fc89ced1907b42018ad2ef0f45444bfb
Andre Höfer <bl4ckb1rd1977@googlemail.com>
2024-07-12 06:39:44 +0200

    no tree on startup for quick file edit

--- lua/polish.lua
+++ lua/polish.lua
@@ -19,9 +19,10 @@
 
 -- autostart nvim-tree
 --
-vim.api.nvim_create_autocmd("VimEnter", {
-  command = "set nornu nonu | Neotree toggle",
-})
+-- vim.api.nvim_create_autocmd("VimEnter", {
+--   command = "set nornu nonu",
+--   -- command = "set nornu nonu | Neotree toggle",
+-- })
 vim.api.nvim_create_autocmd("BufEnter", {
   command = "set nornu nu",
 })
